b023bd37b047333246669433fa36c2556f3143a2
Safiro <altxafiro@gmail.com>
2025-01-29 23:56:52 +0300

    fix(ui): Fix alignment

--- src/pages/home/ui/Page.tsx
+++ src/pages/home/ui/Page.tsx
@@ -45,7 +45,7 @@ export function Page() {
         ></div>
 
         <div className="flex h-screen max-h-[1400px] min-h-[400px] justify-center bg-black text-white mix-blend-exclusion">
-          <div className="flex w-full max-w-[1120px] justify-center pl-[20px] pr-[20px] lg:mr-[550px]">
+          <div className="flex w-full max-w-[1120px] justify-center pl-[20px] pr-[20px] lg:mr-[600px]">
             <div className="flex flex-col items-center gap-[10px] lg:flex-row">
               <SvgIcon
                 className="mt-0 h-[74px] w-[72px] lg:mt-[20px]"
@@ -65,15 +65,15 @@ export function Page() {
         <div className="h-[70vh] max-h-[1000px] min-h-[300px] w-full bg-white mix-blend-exclusion"></div>
 
         <div className="flex h-screen max-h-[1400px] min-h-[400px] items-center justify-center bg-white text-black mix-blend-exclusion">
-          <div className="flex w-full max-w-[1120px] justify-center pl-[20px] pr-[20px] lg:ml-[550px]">
+          <div className="flex w-full max-w-[1120px] justify-center pl-[20px] pr-[20px] lg:ml-[600px]">
             <div className="flex flex-col items-center gap-[10px] lg:flex-row">
-              <p className="max-w-[160px] text-center lg:text-left">
+              <p className="max-w-[160px] text-center lg:text-right">
                 <span className="text-[90px] leading-none">0</span>
                 <br /> Unwanted
                 <br /> Nightcalls
               </p>
               <SvgIcon
-                className="mt-0 h-[73px] w-[73px] lg:mt-[20px]"
+                className="-order-1 mt-0 h-[73px] w-[73px] lg:order-none lg:mt-[20px]"
                 icon="no-calls"
               />
             </div>
